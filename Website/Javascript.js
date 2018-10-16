ac56c4c0d5cb46f7485a577d8d670b0ac492aae0
Aron <aron.smit@hva.nl>
2018-10-16 16:36:29 +0200

    - Hamburger menu op kleiner beeldscherm. -Javascript toegevoegd. -Lettertypes toegevoegd

---
+++ Website/Javascript.js
@@ -0,0 +1,22 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+
+/*eslint-env browser*/
+
+/*eslint 'no-console': 0*/
+
+var hamburger = document.querySelector('nav li');
+
+function klapOpen() {
+    var nav = document.querySelector('nav li:nth-child(2)');
+    nav.style.display = 'block';
+    nav = document.querySelector('nav li:nth-child(3)');
+    nav.style.display = 'block';
+    nav = document.querySelector('nav li:nth-child(4)');
+    nav.style.display = 'block';
+    nav = document.querySelector('nav li:nth-child(5)');
+    nav.style.display = 'block';
+    nav = document.querySelector('nav li:nth-child(6)');
+    nav.style.display = 'block';
+}
+
+hamburger.addEventListener('click', klapOpen);
